0678ba5e0db359baefac1454e6450d24443aa963
luna <1968669819@qq.com>
2025-08-13 14:11:11 +0800

    修改

--- reactor/CMakeFiles/reactor_server.dir/DependInfo.cmake
+++ reactor/CMakeFiles/reactor_server.dir/DependInfo.cmake
@@ -8,7 +8,7 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "/home/luna/reactor/Main.cpp" "CMakeFiles/reactor_server.dir/Main.cpp.o" "gcc" "CMakeFiles/reactor_server.dir/Main.cpp.o.d"
+  "/home/luna/practice/reactor/Main.cpp" "CMakeFiles/reactor_server.dir/Main.cpp.o" "gcc" "CMakeFiles/reactor_server.dir/Main.cpp.o.d"
   )
 
 # Targets to which this target links which contain Fortran sources.
--- reactor/cmake_install.cmake
+++ reactor/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/luna/reactor
+# Install script for directory: /home/luna/practice/reactor
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/luna/reactor/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/luna/practice/reactor/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
